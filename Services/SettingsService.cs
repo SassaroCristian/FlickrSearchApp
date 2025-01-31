9c804da76100b444e7ddfed71f94b586ed957603
SassaroCristian <sassaro01cristian@gmail.com>
2025-01-31 11:06:16 +0100

    Implement SettingsService for saving and retrieving API key using Preferences instead of appsetting.json

---
+++ Services/SettingsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace FlickrSearchApp.Services
+{
+	public class SettingsService
+	{
+		private const string ApiKey = "2c7633b7724f52403deb5c0192aa0e21";
+
+        public void SaveApiKey(string apiKey)
+        {
+            Preferences.Set(ApiKey, apiKey);
+
+        }
+
+        public string GetApiKey()
+        {
+            return Preferences.Get(ApiKey, string.Empty); // Default is an empty string if not set
+        }
+    }
+
+	
+}
+
