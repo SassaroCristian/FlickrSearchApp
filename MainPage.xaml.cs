95fecee112993cf861faf72055066da74caf5c09
SassaroCristian <sassaro01cristian@gmail.com>
2025-01-31 11:03:29 +0100

    Fix dependency injection in MainPage constructor. Inject FlickrService into MainViewModel.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,16 @@
 ﻿namespace FlickrSearchApp;
 using Microsoft.Maui.Controls;
  using FlickrSearchApp.Models;
+ using FlickrSearchApp.Services;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-		BindingContext = new MainViewModel();
-	}
+        var flickrService = DependencyService.Get<FlickrService>();
+        var viewModel = new MainViewModel(flickrService); 
+        this.BindingContext = viewModel;
+    }
 }
 
