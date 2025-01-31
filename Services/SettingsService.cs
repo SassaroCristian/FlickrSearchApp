959e3db193cd66bb83c27af46a1ebe4b63f6b8c7
SassaroCristian <sassaro01cristian@gmail.com>
2025-01-31 12:10:49 +0100

    Ensure API key initialization at app startup

--- Services/SettingsService.cs
+++ Services/SettingsService.cs
@@ -3,18 +3,14 @@ namespace FlickrSearchApp.Services
 {
 	public class SettingsService
 	{
-		private const string ApiKey = "2c7633b7724f52403deb5c0192aa0e21";
+        const string API_KEY_NAME = "flickr_api_key"; // Key name for storage
+        const string DEFAULT_API_KEY = "2c7633b7724f52403deb5c0192aa0e21";
 
-        public void SaveApiKey(string apiKey)
-        {
-            Preferences.Set(ApiKey, apiKey);
+        public void SaveApiKey(string apiKey) =>
+        Preferences.Set(API_KEY_NAME, apiKey);
 
-        }
-
-        public string GetApiKey()
-        {
-            return Preferences.Get(ApiKey, string.Empty); // Default is an empty string if not set
-        }
+        public string GetApiKey() =>
+            Preferences.Get(API_KEY_NAME, DEFAULT_API_KEY);
     }
 
 	
