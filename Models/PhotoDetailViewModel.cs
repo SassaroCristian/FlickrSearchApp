8a8ab04a45026781fdf0d18a2e89de64fe107b64
SassaroCristian <sassaro01cristian@gmail.com>
2025-02-03 11:43:26 +0100

    added PhotoDetailViewModel to handle photo details
    - Created a ViewModel to store and update the selected photo's details.
    - Implemented INotifyPropertyChanged for UI data binding.
    - The ViewModel receives photo data (URL, title, author) from MainPage.
    - This will be used to bind data to PhotoDetailPage for displaying full-size images.

---
+++ Models/PhotoDetailViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FlickrSearchApp.Models
+{
+	public class PhotoDetailViewModel : INotifyPropertyChanged
+	{
+		private string _photoUrl;
+		private string _title;
+		private string _author;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+        public string PhotoUrl
+        {
+            get => _photoUrl;
+            set
+            {
+                _photoUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set
+            {
+                _author = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PhotoDetailViewModel(string photoUrl, string title, string author)
+        {
+            PhotoUrl = photoUrl;
+            Title = title;
+            Author = author;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
