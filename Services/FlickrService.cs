96bec2947463583659dcd022407fc436d972bf60
SassaroCristian <sassaro01cristian@gmail.com>
2025-02-03 11:25:07 +0100

    Limit Flickr API results to 5 per page

--- Services/FlickrService.cs
+++ Services/FlickrService.cs
@@ -29,19 +29,20 @@ namespace FlickrSearchApp.Services
 
         // Method to search for photos based on a query and page number
         public async Task<FlickrResponse> SearchPhotosAsync(string query, int page = 1)
-		{
-			if (string.IsNullOrEmpty(_apiKey))
-			{
+        {
+            if (string.IsNullOrEmpty(_apiKey))
+            {
                 throw new InvalidOperationException("API key is not set.");
             }
 
             // Construct the Api request url
             var url = $"{ApiUrl}?method=flickr.photos.search" +
-          $"&api_key={_apiKey}" +
-          $"&text={Uri.EscapeDataString(query)}" +
-          $"&page={page}" +
-          "&extras=url_s,url_l" + 
-          "&format=json&nojsoncallback=1";
+                     $"&api_key={_apiKey}" +
+                     $"&text={Uri.EscapeDataString(query)}" +
+                     $"&page={page}" +
+                     "&per_page=5" +  
+                     "&extras=url_s,url_l" +
+                     "&format=json&nojsoncallback=1";
 
             // Send the request and get the response as a string
             var response = await _httpClient.GetStringAsync(url);
@@ -49,7 +50,7 @@ namespace FlickrSearchApp.Services
             // Deserialize the JSON response into a FlickrResponse object
             var result = JsonConvert.DeserializeObject<FlickrResponse>(response);
 
-			return result;
+            return result;
         }
     }
 }
